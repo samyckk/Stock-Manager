eb2febfaf08d3398cd348941534f58e33356e797
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-07-14 13:08:34 +0530

    Update ViewStock.jsx

--- src/components/ViewStock.jsx
+++ src/components/ViewStock.jsx
@@ -3,12 +3,15 @@ import { useEffect, useState } from "react";
 
 const ViewStock = () => {
     const [allStock, setAllStock] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchStock = async () => {
+            setLoading(true);
             await axios.get("https://stock-manager-server.vercel.app/stocks").then((res) => {
                 console.log(res.data);
                 setAllStock(res.data);
+                setLoading(false);
             });
         };
 
@@ -17,30 +20,39 @@ const ViewStock = () => {
 
     return (
         <div className="h-full w-full flex flex-col justify-center items-center mt-24">
-            <h1 className="font-bold text-2xl">All Stock</h1>
-            
-            <table className="border border-black mt-4">
-                <thead>
-                    <tr>
-                        <th className="px-3 py-1">Item</th>
-                        <th className="px-3 py-1">Weight</th>
-                        <th className="px-3 py-1">Purchase</th>
-                        <th className="px-3 py-1">Total Purchase</th>
-                    </tr>
-                </thead>
+            <h1 className="font-bold text-2xl">सारे Stock देखे</h1>
 
-                <tbody>
-                    {
-                    allStock.map((itr, index) => (
-                        <tr key={index}>
-                            <td className="px-3 py-1 text-center">{itr.item}</td>
-                            <td className="px-3 py-1 text-center">{itr.weight}kg</td>
-                            <td className="px-3 py-1 text-center">{itr.purchase.toFixed(2)}/-</td>
-                            <td className="px-3 py-1 text-center">{(itr.weight * itr.purchase).toFixed(2)} Rs.</td>
+            {
+                loading ? (<div><img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="" srcset="" /></div>) :
+
+                (
+                    <>
+                    <table className="border border-black mt-4">
+                    <thead>
+                        <tr>
+                            <th className="px-3 py-1">Item</th>
+                            <th className="px-3 py-1">Weight</th>
+                            <th className="px-3 py-1">Purchase</th>
+                            <th className="px-3 py-1">Total Purchase</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+    
+                    <tbody>
+                        {
+                        allStock.map((itr, index) => (
+                            <tr key={index}>
+                                <td className="px-3 py-1 text-center">{itr.item}</td>
+                                <td className="px-3 py-1 text-center">{(itr.weight).toFixed(2)}kg</td>
+                                <td className="px-3 py-1 text-center">{itr.purchase.toFixed(2)}/-</td>
+                                <td className="px-3 py-1 text-center">{(itr.weight * itr.purchase).toFixed(2)} Rs.</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </>)
+            }
+            
+            
             
         </div>
     );
